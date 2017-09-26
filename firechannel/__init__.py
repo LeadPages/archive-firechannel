340c77fb1651ad5cd94033c37743dcf8debede48
Bogdan Popa <popa.bogdanp@gmail.com>
2017-09-26 17:21:08 +0300

    Bump version: 0.3.2 → 0.4.0

--- firechannel/__init__.py
+++ firechannel/__init__.py
@@ -3,4 +3,4 @@ from .credentials import get_credentials  # noqa
 from .firebase import Firebase  # noqa
 from .pool import Pool, ThreadLocalPool  # noqa
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
